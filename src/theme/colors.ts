64b151ca25fb90389c39ca48465a8988d8e942f7
frankydev1 <72719344@users.noreply.github.com>
2021-06-03 23:44:06 -0400

    updates

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -50,7 +50,7 @@ export const lightColors: Colors = {
   secondary: "#9A6AFF",
   background: "#000000",
   backgroundDisabled: "#3c3742",
-  backgroundAlt: "#eee7fa",
+  backgroundAlt: "#000000",
   cardBorder: "#383241",
   contrast: "#FFFFFF",
   dropdown: "#1E1D20",
